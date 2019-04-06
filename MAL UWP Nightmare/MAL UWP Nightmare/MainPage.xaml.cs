6fd405e6b513a4ce32c04f6a4bc43b6bcebb8463
Fokje Moer <Pretender0906@gmail.com>
2019-04-06 02:21:21 +0200

    Messed around with some UI stuff and fixed Debug panic. Added some code and example stuff to the project

--- MAL UWP Nightmare/MAL UWP Nightmare/MainPage.xaml.cs
+++ MAL UWP Nightmare/MAL UWP Nightmare/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +23,25 @@ namespace MAL_UWP_Nightmare
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private bool bgflipped = false;
+
         public MainPage()
         {
             this.InitializeComponent();
+            (Window.Current.Content as Frame).Background = new ImageBrush { Stretch = Stretch.Fill, ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/StoreLogo.png")) };
+            (Window.Current.Content as Frame).SizeChanged += TapHandle;
+        }
+
+        private void TapHandle(object sender, SizeChangedEventArgs e)
+        {
+            if (bgflipped)
+            {
+                (Window.Current.Content as Frame).Background = new ImageBrush { Stretch = Stretch.Fill, ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/PokeBG.png")) };
+            } else
+            {
+                (Window.Current.Content as Frame).Background = new ImageBrush { Stretch = Stretch.Fill, ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/SplashScreen.scale-200.png")) };
+            }
+            bgflipped = !bgflipped;
         }
     }
 }
