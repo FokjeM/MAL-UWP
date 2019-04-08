e5225213f6eebdaa1a674f5fdba100d61bab81fe
Unknown <mac.kok@outlook.com>
2019-04-08 03:01:48 +0200

    Early commit

--- MAL UWP Nightmare/MAL UWP Nightmare/SearchPage.cs
+++ MAL UWP Nightmare/MAL UWP Nightmare/SearchPage.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace MAL_UWP_Nightmare
 {
-    class SearchPage
+    public class SearchPage : IPage
     {
+        private List<SearchResult> _results;
+        public List<SearchResult> results
+        {
+            get
+            {
+                return _results;
+            }
+        }
+        private object _observer; //To do: Change object to type Main.
+        public object observer
+        {
+            get
+            {
+                return _observer;
+            }
+        }
+        public bool IsLocal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SavePage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetContent(JObject json)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetErrorContent(string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+        private void notifyObserver()
+        {
+
+        }
     }
 }
